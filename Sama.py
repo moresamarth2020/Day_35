b0ca533a321e9b37e6dc23fb58865adb9fc87850
More Samarth Nagesh <115178193+moresamarth2020@users.noreply.github.com>
2025-05-29 11:23:31 +0530

    Add files via upload

---
+++ Sama.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def Wellcome():
+    print("Hey You are Wellcome My Frinds")
+    
+if __name__ == "__main__":
+    Wellcome()
+
+
+# In[ ]:
+
+
+
+
